ab7d6ef1d924bbfb7d38017b9f2a2a67f5f23110
Nycolas Galdino <nycolas@mg8.com.br>
2023-10-19 11:20:32 -0300

    first commit

---
+++ binary_tree.py
@@ -0,0 +1,22 @@
+class Tree:
+    def __init__(self, key = None, left = None, right = None):
+        self.key = key
+        self.left = left
+        self.right = right
+
+    def insert(self):
+        pass
+    
+    def pre_order(self):
+        pass
+    
+    def in_order(self):
+        pass
+    
+    def post_order(self):
+        pass
+
+
+tree = Tree(50)
+
+print(tree)
